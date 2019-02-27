794b4edf4b53d94f3ce926171598fe4c1a5dbd92
Nirmala <nirmala.talari@gmail.com>
2019-02-27 16:31:45 -0500

    quic

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,20 +1,29 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.HashMap;
+
 public class Router {
+    HashMap<String, String> str = new HashMap();
+
     public void add(String path, String controller) {
+        str.put(path,controller);
     }
 
     public Integer size() {
-        return null;
+
+        return str.size();
     }
 
     public String getController(String path) {
-        return null;
+
+        return str.toString();
     }
 
     public void update(String path, String studentController) {
+
     }
 
     public void remove(String path) {
+        str.remove(path);
     }
 }
